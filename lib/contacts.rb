5f6d2fcf64466574920fb97557640d98829cadd1
James Higgins and Nick Powell <james@>
2017-04-27 10:53:06 -0700

    add class phone and get new contact to contain personal phone

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -6,11 +6,14 @@ class Contacts
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :job_title
+  attr_accessor :phone
 
-  def initialize (first_name, last_name, job_title)
+
+  def initialize (first_name, last_name, job_title, phone)
     self.first_name = first_name
     self.last_name = last_name
     self.job_title = job_title
+    self.phone = Phone.new(phone)
   end
 
   def Contacts.all
@@ -24,5 +27,25 @@ class Contacts
   def Contacts.clear
     @@contacts_array = []
   end
-  
+end
+
+
+
+
+class Phone
+  @@phone_array = []
+
+  attr_accessor :personal
+  # attr_accessor :cell
+  # attr_accessor :home
+
+  def initialize(personal)
+    self.personal = personal
+    # self.cell = cell
+    # self.home = home
+  end
+
+  def Phone.all
+    @@phone_array
+  end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -6,55 +6,78 @@ describe(Contacts) do
   # before() do
   #   Contacts.clear
   # end
-
-
-  describe('#initialize') do
-    it('returns the first name of a new instance of the Contacts') do
-      new_contact = Contacts.new('Nick', 'Powell', 'Student')
-      expect(new_contact.first_name).to(eq('Nick'))
-      end
-
-    it('returns the first name of a new instance of the Contacts') do
-      new_contact = Contacts.new('Nick', 'Powell', 'Student')
-      expect(new_contact.last_name).to(eq('Powell'))
-      end
-
-    it('returns the first name of a new instance of the Contacts') do
-      new_contact = Contacts.new('Nick', 'Powell', 'Student')
-      expect(new_contact.job_title).to(eq('Student'))
-      end
-    end
-
-    describe('.all') do
-      it('returns all stored contacts in the all_contacts array') do
-        new_contact = Contacts.new('Nick', 'Powell', 'Student')
-        expect(Contacts.all).to(eq([]))
-        end
-    end
-
-    describe('.all') do
-      it('returns all stored contacts in the all_contacts array') do
-        new_contact = Contacts.new('Nick', 'Powell', 'Student')
-        new_contact.save
-        expect(Contacts.all).to(eq([new_contact]))
-        end
-    end
-    describe('.all') do
-      it('clears all stored contacts in all_contacts array') do
-        new_contact = Contacts.new('Nick', 'Powell', 'Student')
-        new_contact.save
-        Contacts.clear
-        expect(Contacts.all).to(eq([]))
-        end
-    end
-    describe('.all') do
-      it('returns second stored contact in the all_contacts array') do
-        new_contact = Contacts.new('Nick', 'Powell', 'Student')
-        new_contact.save
-        new_contact2 = Contacts.new('James', 'Higgins', 'Student')
-        new_contact2.save
-        expect(new_contact2.first_name).to(eq("James"))
-        end
-    end
-
+  # describe('#initialize') do
+  #   it('returns the first name of a new instance of the Contacts') do
+  #     new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #     expect(new_contact.first_name).to(eq('Nick'))
+  #     end
+  #
+  #   it('returns the first name of a new instance of the Contacts') do
+  #     new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #     expect(new_contact.last_name).to(eq('Powell'))
+  #     end
+  #
+  #   it('returns the first name of a new instance of the Contacts') do
+  #     new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #     expect(new_contact.job_title).to(eq('Student'))
+  #     end
+  #   end
+  #
+  #   describe('.all') do
+  #     it('returns all stored contacts in the all_contacts array') do
+  #       new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #       expect(Contacts.all).to(eq([]))
+  #       end
+  #   end
+  #
+  #   describe('.all') do
+  #     it('returns all stored contacts in the all_contacts array') do
+  #       new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #       new_contact.save
+  #       expect(Contacts.all).to(eq([new_contact]))
+  #       end
+  #   end
+  #   describe('.all') do
+  #     it('clears all stored contacts in all_contacts array') do
+  #       new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #       new_contact.save
+  #       Contacts.clear
+  #       expect(Contacts.all).to(eq([]))
+  #       end
+  #   end
+  #   describe('.all') do
+  #     it('returns second stored contact in the all_contacts array') do
+  #       new_contact = Contacts.new('Nick', 'Powell', 'Student')
+  #       new_contact.save
+  #       new_contact2 = Contacts.new('James', 'Higgins', 'Student')
+  #       new_contact2.save
+  #       expect(new_contact2.first_name).to(eq("James"))
+  #       end
+ it('returns the phone array for an instance of a new contact') do
+    new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
+    new_contact.save
+    expect(Contacts.all).to(eq([new_contact]))
+  end
+ it('returns the phone array for an instance of a new contact') do
+    new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
+    new_contact.save
+    expect(new_contact.phone.personal).to(eq("503"))
+  end
 end
+
+
+
+
+  describe(Phone) do
+    it('returns the phone array for an instance of a new contact') do
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
+      expect(new_contact.phone.personal).to(eq("503"))
+    end
+    it('returns the phone array for an instance of a new contact') do
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
+      expect(Phone.all).to(eq([]))
+    end
+  end
+
+
+# end
