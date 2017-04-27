223e8198284a02aff7d1eb754e688d585f65026e
James Higgins and Nick Powell <james@>
2017-04-27 12:00:55 -0700

    add email class for second object within object

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -7,13 +7,16 @@ class Contacts
   attr_accessor :last_name
   attr_accessor :job_title
   attr_accessor :phone_array
+  attr_accessor :email_array
 
 
-  def initialize (first_name, last_name, job_title, phone_params)
+
+  def initialize (first_name, last_name, job_title, phone_params, email_params)
     self.first_name = first_name
     self.last_name = last_name
     self.job_title = job_title
     self.phone_array = [Phone.new(phone_params.fetch(:type), phone_params.fetch(:number))]
+    self.email_array = [Email.new(email_params.fetch(:type), email_params.fetch(:email))]
 
   end
 
@@ -23,6 +26,9 @@ class Contacts
 
   def add_phone (phone_params)
     self.phone_array.push(Phone.new(phone_params.fetch(:type), phone_params.fetch(:number)))
+  end
+  def add_email (email_params)
+    self.email_array.push(Email.new(email_params.fetch(:type), email_params.fetch(:email)))
   end
 
   def save
@@ -44,6 +50,15 @@ class Phone
   def initialize(type, number)
     self.type = type
     self.number = number
+  end
+end
 
+class Email
+  attr_accessor :type
+  attr_accessor :email
+
+  def initialize(type, email)
+    self.type = type
+    self.email = email
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -9,19 +9,22 @@ describe(Contacts) do
   describe('#initialize') do
     it('returns the first name of a new instance of the Contacts') do
       test_phone_params = {:type => "personal", :number => "5037730029"}
-      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+      test_email_params = {:type => "work", :email => "test@gmail.com"}
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
       expect(new_contact.first_name).to(eq('Nick'))
       end
 
     it('returns the first name of a new instance of the Contacts') do
       test_phone_params = {:type => "personal", :number => "5037730029"}
-      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+      test_email_params = {:type => "work", :email => "test@gmail.com"}
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
       expect(new_contact.last_name).to(eq('Powell'))
       end
 
     it('returns the first name of a new instance of the Contacts') do
       test_phone_params = {:type => "personal", :number => "5037730029"}
-      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+      test_email_params = {:type => "work", :email => "test@gmail.com"}
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
       expect(new_contact.job_title).to(eq('Student'))
       end
     end
@@ -29,20 +32,23 @@ describe(Contacts) do
     describe('.all') do
       it('returns all stored contacts in the all_contacts array') do
         test_phone_params = {:type => "personal", :number => "5037730029"}
-        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+        test_email_params = {:type => "work", :email => "test@gmail.com"}
+        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
         expect(Contacts.all).to(eq([]))
         end
 
       it('returns all stored contacts in the all_contacts array') do
         test_phone_params = {:type => "personal", :number => "5037730029"}
-        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+        test_email_params = {:type => "work", :email => "test@gmail.com"}
+        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
         new_contact.save
         expect(Contacts.all).to(eq([new_contact]))
         end
 
       it('clears all stored contacts in all_contacts array') do
         test_phone_params = {:type => "personal", :number => "5037730029"}
-        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+        test_email_params = {:type => "work", :email => "test@gmail.com"}
+        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
         new_contact.save
         Contacts.clear
         expect(Contacts.all).to(eq([]))
@@ -50,36 +56,28 @@ describe(Contacts) do
 
       it('returns second stored contact in the all_contacts array') do
         test_phone_params = {:type => "personal", :number => "5037730029"}
-        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
+        test_email_params = {:type => "work", :email => "test@gmail.com"}
+        new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
         new_contact.save
-        new_contact2 = Contacts.new('James', 'Higgins', 'Student', test_phone_params)
+        new_contact2 = Contacts.new('James', 'Higgins', 'Student', test_phone_params, test_email_params)
         new_contact2.save
         expect(new_contact2.first_name).to(eq("James"))
         end
       end
 
- it('returns the phone array for an instance of a new contact') do
-    test_phone_params = {:type => "personal", :number => "5037730029"}
-    new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params)
-    new_contact.save
-    expect(Contacts.all[0].phone_array[0].number).to(eq("5037730029"))
+   it('returns the phone number for an instance of a new contact') do
+      test_phone_params = {:type => "personal", :number => "5037730029"}
+      test_email_params = {:type => "work", :email => "test@gmail.com"}
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
+      new_contact.save
+      expect(Contacts.all[0].phone_array[0].number).to(eq("5037730029"))
+  end
+   it('returns the email address for an instance of a new contact') do
+      test_email_params = {:type => "work", :email => "test@gmail.com"}
+      test_phone_params = {:type => "personal", :number => "5037730029"}
+      new_contact = Contacts.new('Nick', 'Powell', 'Student', test_phone_params, test_email_params)
+      new_contact.save
+      expect(Contacts.all[0].email_array[0].email).to(eq("test@gmail.com"))
   end
 
 end
-
-
-  #
-  #
-  # describe(Phone) do
-  #   it('returns the phone array for an instance of a new contact') do
-  #     new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
-  #     expect(new_contact.phone.personal).to(eq("503"))
-  #   end
-  #   it('returns the phone array for an instance of a new contact') do
-  #     new_contact = Contacts.new('Nick', 'Powell', 'Student', '503')
-  #     expect(Phone.all).to(eq([]))
-  #   end
-  # end
-
-
-# end
