396474af16fb10a1511138172a7cf17856ecef78
James Higgins and Nick Powell <james@>
2017-04-27 09:48:16 -0700

    test and pass second spec for all array. Returns empty array.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,8 @@
 
 class Contacts
 
+  @@contacts_array = []
+
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :job_title
@@ -9,6 +11,9 @@ class Contacts
     self.first_name = first_name
     self.last_name = last_name
     self.job_title = job_title
+  end
 
+  def Contacts.all
+    @@contacts_array
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,18 +2,34 @@ require "contacts"
 require "rspec"
 require "pry"
 
+describe(Contacts) do
+  # before() do
+  #   Contacts.clear
+  # end
 
-describe('Contacts#initialize') do
-  it('returns the first name of a new instance of the Contacts') do
-    new_contact = Contacts.new('Nick', 'Powell', 'Student')
-    expect(new_contact.first_name).to(eq('Nick'))
+
+  describe('#initialize') do
+    it('returns the first name of a new instance of the Contacts') do
+      new_contact = Contacts.new('Nick', 'Powell', 'Student')
+      expect(new_contact.first_name).to(eq('Nick'))
+      end
+
+    it('returns the first name of a new instance of the Contacts') do
+      new_contact = Contacts.new('Nick', 'Powell', 'Student')
+      expect(new_contact.last_name).to(eq('Powell'))
+      end
+
+    it('returns the first name of a new instance of the Contacts') do
+      new_contact = Contacts.new('Nick', 'Powell', 'Student')
+      expect(new_contact.job_title).to(eq('Student'))
+      end
     end
-  it('returns the first name of a new instance of the Contacts') do
-    new_contact = Contacts.new('Nick', 'Powell', 'Student')
-    expect(new_contact.last_name).to(eq('Powell'))
+
+    describe('.all') do
+      it('returns all stored contacts in the all_contacts array') do
+        new_contact = Contacts.new('Nick', 'Powell', 'Student')
+        expect(Contacts.all).to(eq([]))
+        end
     end
-  it('returns the first name of a new instance of the Contacts') do
-    new_contact = Contacts.new('Nick', 'Powell', 'Student')
-    expect(new_contact.job_title).to(eq('Student'))
-    end
-  end
+
+end
